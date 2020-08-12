911f063c3fd8a3d29a356bc3bbeab8e6408e509f
Latezki <kovanen.lauri@gmail.com>
2020-08-12 17:22:01 +0300

    Fixing pictures for the github pages

--- src/albums.js
+++ src/albums.js
@@ -1,77 +1,75 @@
-import {v4 as uuidv4} from 'uuid';
-
 const albums = [
     {
         id: 1,
         artist: "Trivium",
         album: "What the Dead Men Say",
-        albumimg: "/images/trivium_whatthedeadmensay.jpg"
+        albumimg: "./images/trivium_whatthedeadmensay.jpg"
     },
     {      
         id: 2,
         artist: "Viscera",
         album: "Obsidian",
-        albumimg: "/images/viscera_obsidian.jpg"
+        albumimg: "./images/viscera_obsidian.jpg"
     },
     { 
         id: 3,
         artist: "Tk from Ling Tosite Sigure",
         album: "sainou",
-        albumimg: "/images/tkfromlingtositesigure_sainou.jpg"
+        albumimg: "./images/tkfromlingtositesigure_sainou.jpg"
     },
     {
         id: 4,
         artist: "Hollywood Undead",
         album: "Five",
-        albumimg: "/images/hollywoodundead_five.jpg"
+        albumimg: "./images/hollywoodundead_five.jpg"
     },
     {
         id: 5,
         artist: "Hollywood Undead",
         album: "New Empire",
-        albumimg: "/images/hollywoodundead_newempire.jpg"
+        albumimg: "./images/hollywoodundead_newempire.jpg"
     },
     {
         id: 6,
         artist: "Ling Tosite Sigure",
         album: "Es or S",
-        albumimg: "/images/lingtositesigure_esors.jpg"
+        albumimg: "./images/lingtositesigure_esors.jpg"
     },
     {
         id: 7,
         artist: "Our Last Night",
         album: "Let Light Overcome",
-        albumimg: "/images/ourlastnight_letlightovercome.jpg"
+        albumimg: "./images/ourlastnight_letlightovercome.jpg"
     },
     {
         id: 8,
         artist: "Our Last Night",
         album: "Overcome the Darkness",
-        albumimg: "/images/ourlastnight_overcomethedarkness.jpg"
+        albumimg: "./images/ourlastnight_overcomethedarkness.jpg"
     },
     {
         id: 9,
         artist: "Polaris",
         album: "The Death of Me",
-        albumimg: "/images/polaris_thedeathofme.png"
+        albumimg: "./images/polaris_thedeathofme.png"
     },
     {
         id: 10,
         artist: "Trivium",
         album: "Ascendancy",
-        albumimg: "/images/trivium_ascendancy.jpg"
+        albumimg: "./images/trivium_ascendancy.jpg"
     },
     {
         id: 11,
         artist: "Trivium",
         album: "Shogun",
-        albumimg: "/images/trivium_shogun.jpg"
+        albumimg: "./images/trivium_shogun.jpg"
     },
     {
         id: 12,
         artist: "Veil of Maya",
         album: "False Idols",
-        albumimg: "/images/veilofmaya_falseidols.jpg"
+        albumimg: "./images/veilofmaya_falseidols.jpg"
     }
 ]
 
